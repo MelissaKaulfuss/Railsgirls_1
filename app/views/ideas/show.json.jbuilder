3b971ef02d9c4be065a7020c869a510453e4691a
Melissa Kaulfuss <melissa@icelab.com.au>
2013-08-10 16:11:04 +1000

    initial commit

---
+++ app/views/ideas/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @idea, :name, :string, :description, :picture, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = '66f63769f5d70bbb39fd9cd5840916b88f1ceebc00310c80bad56ec1348f4fa14163ce9a76f3736448fd6517c49ca709414a37337da4c83c6ceafb2a1ac4a613'
